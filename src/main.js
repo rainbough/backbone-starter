f2a6fd32778a87e90d0a15e4b601d8c0ff77b29e
Gilbert <gilbertbgarza@gmail.com>
2013-10-07 11:33:38 -0500

    Added missing function wrapper

--- src/main.js
+++ src/main.js
@@ -1,18 +1,21 @@
+(function () {
 
-var shirtOne = new Shirt({ color: 'blue', large: 5, small: 8 });
-var shirtTwo = new Shirt({ color: 'green', large: 2, medium: 11 });
+  var shirtOne = new Shirt({ color: 'blue', large: 5, small: 8 });
+  var shirtTwo = new Shirt({ color: 'green', large: 2, medium: 11 });
 
-var shirts = [shirtOne, shirtTwo];
+  var shirts = [shirtOne, shirtTwo];
 
-// Create a view for every shirt and add that view's element to the page
-_.each(shirts, function (shirt) {
-  var view = new ShirtView({ model: shirt });
-  view.render();
-  $('#shirts').append(view.el);
-});
+  // Create a view for every shirt and add that view's element to the page
+  _.each(shirts, function (shirt) {
+    var view = new ShirtView({ model: shirt });
+    view.render();
+    $('#shirts').append(view.el);
+  });
 
-// Create an inventory view to listen for stock increases.
-var inventory = new InventoryView({
-  el: '.inventory',
-  shirts: shirts
-});
+  // Create an inventory view to listen for stock increases.
+  var inventory = new InventoryView({
+    el: '.inventory',
+    shirts: shirts
+  });
+
+})();
