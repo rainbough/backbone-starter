f0fab2026633c8d4278cd3f347d74841095cd72e
Gilbert <gilbertbgarza@gmail.com>
2013-10-07 11:01:19 -0500

    Fixed a comment typo

--- src/views/inventory-view.js
+++ src/views/inventory-view.js
@@ -8,10 +8,10 @@
     addShirts: function (e) {
       var type = $(e.currentTarget).data('type');
       console.log('Adding one of every', type, 'shirt for', this.options.shirts);
-      // TODO: Write a for loop to iterate through window.shirts and
+      // TODO: Write a for loop to iterate through this.options.shirts and
       // add +1 to each of that shirt's `type` inventory.
       // For example, if type === 'small', then add +1 to every small stock
-      // in each shirt in window.shirts. Use .set(???) for this.
+      // in each shirt in this.options.shirts. Use .set(???) for this.
     }
   });
 
