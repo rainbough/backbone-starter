03edb792d217b35027736b325f7ad6a8342ce5c5
Gilbert <gilbertbgarza@gmail.com>
2013-10-07 08:46:15 -0500

    Partner exercise material

--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
 
-// Remove this line when you start working
-alert('Hello World!');
+var shirtOne = new Shirt({ color: 'blue', large: 5, small: 8 });
+var shirtTwo = new Shirt({ color: 'green', large: 2, medium: 11 });
+
+var shirts = [shirtOne, shirtTwo];
+
+// Create a view for every shirt and add that view's element to the page
+_.each(shirts, function (shirt) {
+  var view = new ShirtView({ model: shirt });
+  view.render();
+  $('#shirts').append(view.el);
+});
+
+// Create an inventory view to listen for stock increases.
+var inventory = new InventoryView({
+  el: '.inventory',
+  shirts: shirts
+});
---
+++ src/views/inventory-view.js
@@ -0,0 +1,18 @@
+(function () {
+
+  window.InventoryView = Backbone.View.extend({
+    events: {
+      'click .add-shirts': 'addShirts'
+    },
+
+    addShirts: function (e) {
+      var type = $(e.currentTarget).data('type');
+      console.log('Adding one of every', type, 'shirt for', this.options.shirts);
+      // TODO: Write a for loop to iterate through window.shirts and
+      // add +1 to each of that shirt's `type` inventory.
+      // For example, if type === 'small', then add +1 to every small stock
+      // in each shirt in window.shirts. Use .set(???) for this.
+    }
+  });
+
+})();
